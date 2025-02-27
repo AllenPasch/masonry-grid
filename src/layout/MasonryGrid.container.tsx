fcb48d263f1a15bda97ac23dea1ffd6e0bde5ffc
Allen Pasch <github@irishcousin.com>
2025-02-27 18:02:30 +0100

    chore: stub out a container and component for the masonry grid

---
+++ src/layout/MasonryGrid.container.tsx
@@ -0,0 +1,8 @@
+import { memo } from "react";
+import MasonryGrid from "./MasonryGrid";
+
+const MasonryGridContainer = () => {
+  return <MasonryGrid />;
+};
+
+export default memo(MasonryGridContainer);
---
+++ src/layout/MasonryGrid.tsx
@@ -0,0 +1,7 @@
+import { memo } from "react";
+
+const MasonryGrid = () => {
+  return <div>Masonry</div>;
+};
+
+export default memo(MasonryGrid);

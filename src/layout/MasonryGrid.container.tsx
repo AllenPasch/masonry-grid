ee6225415f55a4509e218226ea7745062324a437
Allen Pasch <github@irishcousin.com>
2025-02-27 18:19:03 +0100

    feat: try out the pexels api library

--- src/layout/MasonryGrid.container.tsx
+++ src/layout/MasonryGrid.container.tsx
@@ -1,7 +1,18 @@
-import { memo } from "react";
+import { memo, useMemo } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+import createPexelsClient from "@/api/pexels/createPexelsClient";
 import MasonryGrid from "./MasonryGrid";
 
 const MasonryGridContainer = () => {
+  const pexelsClient = useMemo(createPexelsClient, []);
+  const query = useQuery({
+    queryKey: ["curated"],
+    queryFn: () => pexelsClient.photos.curated(),
+  });
+
+  console.log("query", query);
+
   return <MasonryGrid />;
 };
 

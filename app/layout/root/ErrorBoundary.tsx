4bef107d9515c2f16da71241adc5ae937b7d6d2a
Allen Pasch <github@irishcousin.com>
2025-03-02 19:18:30 +0100

    perf: redirecting on error means even less generated js code

--- app/layout/root/ErrorBoundary.tsx
+++ app/layout/root/ErrorBoundary.tsx
@@ -1,33 +1,7 @@
-import { isRouteErrorResponse } from "react-router";
+import { useNavigate } from "react-router";
 
-import type { Route } from "../../+types/root";
+export const ErrorBoundary = () => {
+  const navigate = useNavigate();
 
-export const ErrorBoundary = ({ error }: Route.ErrorBoundaryProps) => {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
-
-  if (isRouteErrorResponse(error)) {
-    const is404 = [403, 404].includes(error.status);
-
-    message = is404 ? "404" : "Error";
-    details = is404
-      ? "The requested page could not be found."
-      : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
-  }
-
-  return (
-    <>
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre>
-          <code>{stack}</code>
-        </pre>
-      )}
-    </>
-  );
+  setTimeout(() => navigate(""), 0);
 };

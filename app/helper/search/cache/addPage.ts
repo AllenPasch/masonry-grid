d252cbd48badd8214bdb4fa88ded4fda90af0b1e
Allen Pasch <github@irishcousin.com>
2025-03-03 22:28:35 +0100

    test: add a test for addPage()

--- app/helper/search/cache/addPage.ts
+++ app/helper/search/cache/addPage.ts
@@ -2,7 +2,6 @@ import { IPhotos } from "~/api/pexels";
 import { fillPage } from "~/helper/grid/page";
 import { cachedPages } from ".";
 
-// TODO: Unit test, like the reducer.
 export const addPage = (
   searchQuery: string,
   pageNumber: number,
